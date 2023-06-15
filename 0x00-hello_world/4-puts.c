93d595d8c5830e4deb185ccc829214c4702fdf3c
onjurefree <fridahonjushs@gmail.com>
2023-06-15 02:33:10 -0700

    6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (seccess)
+ {
+ puts(""Programming is like building a multilingual puzzle");
+ return (0);
+ }
