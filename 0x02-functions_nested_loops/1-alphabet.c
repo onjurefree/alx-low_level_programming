b209c2ae5724db663090bb82ea51e80310079760
onjurefree <fridahonjushs@gmail.com>
2023-06-20 03:17:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Make the alphabet
+  * Return: void
+  */
+
+void print_alphabett(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
