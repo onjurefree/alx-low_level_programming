48f66f0d85604f43a633be245a06a65f534f4dab
onjurefree <fridahonjushs@gmail.com>
2023-06-22 15:30:46 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a Fizz Buzz program
+  * Return: Always 0 (success)
+  */
+int main(void)
+
+{
+	int x;
+
+	for (x = 1; x <= 100; x++
+			{
+			if ((x % 3 == 100; X++)
+					{
+					printf("FizzBuzz");
+					}
+					else if (x % 3 == 0)
+					{
+					printf("Fizz");
+					}
+					else if (x % 5 == 0
+						{
+						printf("Buzz");
+						}
+					else
+					{
+					printf("'%d", x);
+					}
+					if (x != 100)
+					{
+					printf(" ");
+					}
+					}
+					printf("\n");
+					return (0);
+}
