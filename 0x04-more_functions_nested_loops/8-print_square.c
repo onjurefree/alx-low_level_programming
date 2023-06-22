8df306a1e3a40c92bca15b754b673b8ff3f3975e
onjurefree <fridahonjushs@gmail.com>
2023-06-22 16:50:33 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_square - Print n squares according n number of times
+  * print_square - Prints n squares according n number of times
   * @size: The number of squires/number of times
   * Return: empty
   */
