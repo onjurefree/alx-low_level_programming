957b6fcfaae15a6dff32dfe4c82d0e6960224c6e
onjurefree <fridahonjushs@gmail.com>
2023-06-22 17:23:57 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+  * main - Prints the lagest prime factor of the number 512852475143
+  * Return: Always0 (Success)
+  */
+
+int main(void)
+{
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
