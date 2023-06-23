f45b6105a517ffdda75d086a103c96144c903cf3
onjurefree <fridahonjushs@gmail.com>
2023-06-22 17:42:09 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,10 +14,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n *= -1;
-		k = 10;
+		k = n;
 		_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
-		print_number((unsigned int) n % 10 + '0');
+		print_number(k);
+		_putchar((unsigned int) n % 10 + '0');
 }
