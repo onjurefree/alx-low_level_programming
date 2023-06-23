3273b0f8e5cc1f43c5f202be1af53e4752f7774c
onjurefree <fridahonjushs@gmail.com>
2023-06-23 01:49:21 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,24 +6,24 @@
   * Return: empty
   */
 
-void print_squire(int size)
+void print_square(int size)
 
 {
 	int x, y;
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
-		{
-			for (y = 0; y < size; y++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
