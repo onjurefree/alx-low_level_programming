5660eca3c3181033ea16d2e11ee88edf56f91d13
onjurefree <fridahonjushs@gmail.com>
2023-06-26 03:33:48 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _puts - prints a string
+  * _puts - Prints a string
   * @str: The string to print
   * Return: void
   */
@@ -10,7 +10,7 @@ void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(str);
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
