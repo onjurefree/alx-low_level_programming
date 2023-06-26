d9a2ca3d4f1d761aa6f43a5c62183ffe209c2208
onjurefree <fridahonjushs@gmail.com>
2023-06-26 03:41:44 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,14 +12,14 @@ void print_rev(char *s)
 
 	int c = 0;
 
-	while  (s[c] !=n'\0')
+	while  (s[c] != '\0')
 	{
 		c++;
 	}
 
 	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(s[c]);
+	_putchar(s[c]);
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
