8cc115e3b2a447555283e3f04577226bae348a60
onjurefree <fridahonjushs@gmail.com>
2023-06-26 03:48:56 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,13 @@ void rev_string(char *s)
 	int len = 0, index = 0;
 	char tmp;
 
-	While (s[index++])
-		len++;
+	while (s[index++])
+	len++;
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index -1] = tmp;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
