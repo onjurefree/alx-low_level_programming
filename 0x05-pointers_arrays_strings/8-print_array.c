b0d25db100cf6f2e2df71d66a9b3e6b45291213a
onjurefree <fridahonjushs@gmail.com>
2023-06-26 04:33:42 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,11 @@ void print_array(int *a, int n)
 
 	for (j = 0; j < n; j++)
 	{
-		printf("\d", a[j]);
-		if (j != (n - 1))
-		{
-			printf(", ");
-		}
+	printf("%d", a[j]);
+	if (j != (n - 1))
+	{
+	printf(", ");
+	}
 	}
 	printf("\n");
 }
