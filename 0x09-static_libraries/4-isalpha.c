9171f1aee2e7707eb01920d3b2c90f68638d2c70
onjurefree <fridahonjushs@gmail.com>
2023-07-07 13:30:30 -0700

    nm libmy.a

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,23 +1,10 @@
 #include "main.h"
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
+ * _isalpha - Entry point
+ * @c: input
  * Return: Always 0 (Success)
  */
-char *_strpbrk(char *s, char *accept)
+int _isalpha(int c)
 {
-	int k;
 
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-		if (*s == accept[k])
-		return (s);
-		}
-	s++;
-	}
-
-return ('\0');
 }
