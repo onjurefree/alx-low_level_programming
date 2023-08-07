8bd7ef5fb603286143f2c98ee05636f8f5735291
onjurefree <fridahonjushs@gmail.com>
2023-08-07 05:31:18 -0700

    2-append_text_to_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,6 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
+	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
@@ -20,6 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
+
 	free(buf);
 	close(fd);
 	return (w);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,9 @@
 #define main_h
 
 #include <sys/types.h>
-#include <sys/star.h>
-#include < fcntl.h>
-#include <unsistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
