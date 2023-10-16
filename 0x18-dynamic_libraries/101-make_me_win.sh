b14e9f8a44d2b94d7f4b3fb0ed5b74132a867d0d
onjurefree <fridahonjushs@gmail.com>
2023-10-16 06:53:14 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -p /tmt https:/github.com/baccrie/alx_low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -p /tmt https:/github.com/baccrie/alx_low_level_programming
+/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD+?tmp/libmask.so
